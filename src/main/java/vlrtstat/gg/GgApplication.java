bf75400870db64f64f711e53e63bfe9ac5331754
ohtaehyun <othdev95@gmail.com>
2023-10-04 22:13:14 +0900

    Initial commit

---
+++ src/main/java/vlrtstat/gg/GgApplication.java
@@ -0,0 +1,13 @@
+package vlrtstat.gg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vlrtstat/gg/GgApplicationTests.java
@@ -0,0 +1,13 @@
+package vlrtstat.gg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
