d712b068748f6ae9c834c87d4e764c6daf10e37b
ohtaehyun <othdev95@gmail.com>
2023-10-08 21:31:11 +0900

    feat: 랭킹 리더보드 관련 api 구현 ing

---
+++ src/main/java/vlrtstat/gg/riotLeaderBoard/controller/RiotLeaderBoardController.java
@@ -0,0 +1,8 @@
+package vlrtstat.gg.riotLeaderBoard.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/leaderboards")
+public class RiotLeaderBoardController {
+
+}
---
+++ src/main/java/vlrtstat/gg/riotLeaderBoard/service/RiotLeaderBoardService.java
@@ -0,0 +1,8 @@
+package vlrtstat.gg.riotLeaderBoard.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RiotLeaderBoardService {
+
+}
