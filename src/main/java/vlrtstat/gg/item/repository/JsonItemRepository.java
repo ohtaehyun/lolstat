76f94e91ef8397f5f3ec481ad6573bf5c77fe64c
ohtaehyun <othdev95@gmail.com>
2023-10-22 17:29:45 +0900

    feat: jar 파일로 배포시 파일을 못찾는 이슈 수정

--- src/main/java/vlrtstat/gg/item/repository/JsonItemRepository.java
+++ src/main/java/vlrtstat/gg/item/repository/JsonItemRepository.java
@@ -11,6 +11,7 @@ import vlrtstat.gg.item.domain.Item;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 
 @Repository
@@ -20,8 +21,9 @@ public class JsonItemRepository implements ItemRepository {
     public JsonItemRepository() throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
         ClassPathResource classPathResource = new ClassPathResource("data/13.20.1/item.json");
-        Reader reader = new FileReader(classPathResource.getFile());
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(
+                new InputStreamReader(classPathResource.getInputStream(), "UTF-8")
+        );
         itemData = (JSONObject) jsonObject.get("data");
     }
 
