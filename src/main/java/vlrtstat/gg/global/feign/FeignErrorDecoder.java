de16ffd16fe4ba87fd908c2c841ad35bb1747dc1
ohtaehyun <othdev95@gmail.com>
2023-11-03 20:38:31 +0900

    feat: 에러 발생 시 슬랙 알림 추가

---
+++ src/main/java/vlrtstat/gg/global/feign/FeignErrorDecoder.java
@@ -0,0 +1,43 @@
+package vlrtstat.gg.global.feign;
+
+import com.slack.api.Slack;
+import com.slack.api.webhook.Payload;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.rmi.ServerException;
+
+@Component
+public class FeignErrorDecoder implements ErrorDecoder {
+    private Slack slack;
+
+    @Value("${slack.url}")
+    private String webhookUrl;
+
+    public FeignErrorDecoder() {
+        slack = Slack.getInstance();
+    }
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        switch (response.status()) {
+            case 404:
+                try {
+                    Payload payload = Payload.builder().text(
+                            "methodKey: " + methodKey + "\n" +
+                            "reason: " + response.reason() + "\n" +
+                            "url: " + response.request().url()
+                    ).build();
+                    slack.send(webhookUrl, payload);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return new ServerException(response.reason());
+            default:
+                return new Exception(response.reason());
+        }
+    }
+}
