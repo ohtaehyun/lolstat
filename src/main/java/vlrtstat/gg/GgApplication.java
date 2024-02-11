8bd46658a22b85b1f9acfa749e07dd5ad928a6ef
ohtaehyun <othdev95@gmail.com>
2024-02-11 21:34:42 +0900

    feat: 타임존 문제 수정

--- src/main/java/vlrtstat/gg/GgApplication.java
+++ src/main/java/vlrtstat/gg/GgApplication.java
@@ -1,15 +1,22 @@
 package vlrtstat.gg;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableCaching
 public class GgApplication {
-
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GgApplication.class, args);
 	}
